f78a58d0732b7b47bc50ba57fb265af785de1114
espinoza <espinozatest2@gmail.com>
2021-03-10 19:16:02 +0000

    nuevo modulouno

--- djangocalculator/urls.py
+++ djangocalculator/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-from django.confi import settings
-from django.confi.urls.static import static 
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('',include('rootapp.urls')),
