3fb1397253f9e77397269c704d97caf1821f88e5
espinoza <espinozatest2@gmail.com>
2021-03-10 16:41:55 +0000

    version 1.0

--- djangocalculator/settings.py
+++ djangocalculator/settings.py
@@ -118,9 +118,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Update database configuration with $DATABASE_URL.
-import dj_database_url  
-db_from_env = dj_database_url.config(conn_max_age=500)  
-DATABASES['default'].update(db_from_env)
+
 
 
 # Static files (CSS, JavaScript, Images)
@@ -133,4 +131,4 @@ STATICFILES_DIRS = [
 
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
